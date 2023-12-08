d3b493165e564755e4ea76e181d26aa28d20584d
OrlandoSanchez11 <144269090+OrlandoSanchez11@users.noreply.github.com>
2023-12-07 22:02:47 -0500

    Add files via upload

---
+++ src/connection/MyConnection.java
@@ -0,0 +1,23 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+public class MyConnection {
+    public static final String username = "root";
+    public static final String password = "root";
+    public static final String url = "jdbc:mysql://localhost:3306/online_shopping";
+    public static Connection con = null;
+    
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, username, password);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, ""+e, "", JOptionPane.WARNING_MESSAGE);
+        }
+        
+        return con;
+    }
+}
